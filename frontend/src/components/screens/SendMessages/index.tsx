d9df1f87b588a477a94822b1e7a5a823b499a8db
Johnatan <devsantosjohn@gmail.com>
2024-09-24 18:20:55 -0300

    :sparkles: [FEAT] - Adicionando leitura de arquivo csv.

--- frontend/src/components/screens/SendMessages/index.tsx
+++ frontend/src/components/screens/SendMessages/index.tsx
@@ -3,10 +3,14 @@ import style from './SendMessages.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FormEvent, useState } from 'react'
+import { Divider } from '@mui/material'
+import { Loading } from '@/components/_ui/Loading'
 
 export function SendMessagesComponent() {
   const [phones, setPhones] = useState<string[]>([])
   const [phoneInput, setPhoneInput] = useState<string>('')
+  const [csvFile, setCsvFile] = useState<File | null>(null)
+  const [loadingReadCsvFile, setLoadingReadCsvFile] = useState<boolean>(false)
 
   function onAddPhone(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -16,12 +20,49 @@ export function SendMessagesComponent() {
     setPhoneInput('')
   }
 
+  function handleSelectFile() {
+    const input = document.createElement('input')
+
+    input.type = 'file'
+
+    input.onchange = async (event: Event) => {
+      const target = event.target as HTMLInputElement
+
+      const file = (target.files || [])[0] as File
+
+      setCsvFile(file)
+    }
+
+    input.click()
+  }
+
+  function readCsvFile() {
+    setLoadingReadCsvFile(true)
+
+    setTimeout(() => {}, 3000)
+
+    setLoadingReadCsvFile(false)
+  }
+
   return (
     <div>
       <section className={style.phonesContainer}>
-        <button type="button" className={style.selectFileButton}>
-          Importar .csv
-        </button>
+        {csvFile ? (
+          <div>
+            <b>{csvFile.name}</b>
+            <button type="button" onClick={readCsvFile}>
+              Importar
+            </button>
+          </div>
+        ) : (
+          <button
+            type="button"
+            className={style.selectFileButton}
+            onClick={handleSelectFile}
+          >
+            Selecionar .csv
+          </button>
+        )}
 
         <form onSubmit={onAddPhone} className={style.addPhoneForm}>
           <CustomTextField
@@ -29,35 +70,51 @@ export function SendMessagesComponent() {
             label="Telefone"
             placeholder="Digite o telefone"
             value={phoneInput}
+            disabled={loadingReadCsvFile}
             onChange={(event) => {
               setPhoneInput(event.target.value)
             }}
           />
 
-          <button type="button" className={style.addNewPhoneButton}>
+          <button
+            disabled={loadingReadCsvFile}
+            type="button"
+            className={style.addNewPhoneButton}
+          >
             <FontAwesomeIcon icon={faPlus} className={style.icon} />
           </button>
         </form>
 
-        <ul className={style.phonesList}>
-          {phones.map((phone) => {
-            return (
-              <li key={phone} className={style.phoneItem}>
-                <b>{phone}</b>
+        {loadingReadCsvFile ? (
+          <Loading />
+        ) : (
+          <ul className={style.phonesList}>
+            {phones.map((phone) => {
+              return (
+                <li key={phone} className={style.phoneItem}>
+                  <b>{phone}</b>
 
-                <FontAwesomeIcon icon={faTrash} className={style.icon} />
-              </li>
-            )
-          })}
-        </ul>
+                  <FontAwesomeIcon icon={faTrash} className={style.icon} />
+                </li>
+              )
+            })}
+          </ul>
+        )}
       </section>
 
-      <CustomTextField
-        label="Mensagem"
-        placeholder="Digite a mensagem que deseja enviar"
-        multiline
-        rows={5}
-      />
+      <Divider flexItem orientation="vertical" />
+
+      <section className={style.messageContainer}>
+        <CustomTextField
+          label="Mensagem"
+          placeholder="Digite a mensagem que deseja enviar"
+          multiline
+          rows={5}
+        />
+        <button type="button" className={style.sendMessagesButton}>
+          Enviar
+        </button>
+      </section>
     </div>
   )
 }
