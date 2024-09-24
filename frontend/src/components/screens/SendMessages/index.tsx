68705f0c030abc89c020e4a00f8ec1416f0f8ca0
Johnatan <devsantosjohn@gmail.com>
2024-09-24 18:06:39 -0300

    :sparkles: [FEAT] - Monstando estrutura de envio de mensagens em massa.

--- frontend/src/components/screens/SendMessages/index.tsx
+++ frontend/src/components/screens/SendMessages/index.tsx
@@ -1,3 +1,63 @@
+import { CustomTextField } from '@/components/_ui/CustomTextField'
+import style from './SendMessages.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FormEvent, useState } from 'react'
+
 export function SendMessagesComponent() {
-  return <>Send messages</>
+  const [phones, setPhones] = useState<string[]>([])
+  const [phoneInput, setPhoneInput] = useState<string>('')
+
+  function onAddPhone(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    setPhones((oldPhones) => [...oldPhones, phoneInput])
+
+    setPhoneInput('')
+  }
+
+  return (
+    <div>
+      <section className={style.phonesContainer}>
+        <button type="button" className={style.selectFileButton}>
+          Importar .csv
+        </button>
+
+        <form onSubmit={onAddPhone} className={style.addPhoneForm}>
+          <CustomTextField
+            size="small"
+            label="Telefone"
+            placeholder="Digite o telefone"
+            value={phoneInput}
+            onChange={(event) => {
+              setPhoneInput(event.target.value)
+            }}
+          />
+
+          <button type="button" className={style.addNewPhoneButton}>
+            <FontAwesomeIcon icon={faPlus} className={style.icon} />
+          </button>
+        </form>
+
+        <ul className={style.phonesList}>
+          {phones.map((phone) => {
+            return (
+              <li key={phone} className={style.phoneItem}>
+                <b>{phone}</b>
+
+                <FontAwesomeIcon icon={faTrash} className={style.icon} />
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <CustomTextField
+        label="Mensagem"
+        placeholder="Digite a mensagem que deseja enviar"
+        multiline
+        rows={5}
+      />
+    </div>
+  )
 }
